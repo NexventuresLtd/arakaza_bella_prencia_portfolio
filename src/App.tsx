355320b112ca125d5be3edd263596f014e660319
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2025-09-19 23:15:15 +0200

    Done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Home />
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 
+        className="text-4xl font-bold text-gray-800"
+        style={{ fontFamily: 'Arial, sans-serif'
+        }}
+      >
+        Welcome to My Portfolio
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
